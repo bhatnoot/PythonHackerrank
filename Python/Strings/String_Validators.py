8d4de4a099e4c5369e9a872ff37f8950c482094d
bhatnoot <158761003+bhatnoot@users.noreply.github.com>
2024-03-17 17:48:12 +0530

    Update String_Validators.py

--- Python/Strings/String_Validators.py
+++ Python/Strings/String_Validators.py
@@ -63,10 +63,10 @@
 
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-S = raw_input()
+s = input()
+print(any(c.isalnum() for c in s))
+print(any(c.isalpha() for c in s))
+print(any(c.isdigit() for c in s))
+print(any(c.islower() for c in s))
+print(any(c.isupper() for c in s))
 
-print any(c.isalnum() for c in S)
-print any(c.isalpha() for c in S)
-print any(c.isdigit() for c in S)
-print any(c.islower() for c in S)
-print any(c.isupper() for c in S)
